b0f69b77eee861d6d7facc770600e76bd092b6d0
rebb11 <joserebomanguerra@gmail.com>
2023-07-30 15:47:56 +0800

    fix redundancy, organized and cleaned code

--- APPLICANT/applicant_profile.php
+++ APPLICANT/applicant_profile.php
@@ -19,21 +19,6 @@
           include 'function.php';
           include 'sidebar.php';
           ?>
-          <div class="main">
-            <div id="navbar">
-                <a class="active" style="width: 350px; text-align: left;" href="#">Home</a>
-                <a style="width: 350px; padding-top: 20px; margin-left: 80px;">
-                    <formsearch class="example" action="action_page.php">
-                      <center>
-                      <input type="text" placeholder="" name="search">
-                      <button type="submit"><i class="fa fa-search"></i></button>
-                      </center>
-                      
-                  </formsearch>
-                  </a>
-                  
-                <a href="#"></a>
-              </div>
               
               <div class="content">
                 <a style="color:red; margin: 20%;"href="applicant_update.php">Press this to access update profile</a>
