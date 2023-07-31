0ad4fe9c3f39b9ab20a2c7638f0cd376e6c9d7e2
rebb11 <joserebomanguerra@gmail.com>
2023-07-31 18:22:45 +0800

    updated applicant profile

--- APPLICANT/sidebar.php
+++ APPLICANT/sidebar.php
@@ -1,6 +1,7 @@
 <?php
 session_start(); 
 ?>
+<link rel="stylesheet" href="../css/navbar.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 <body>
 <div class="main">
@@ -26,7 +27,7 @@ session_start();
     <a href="applicant_urgent_hiring.php" <?php echo isActivePage("applicant_urgent_hiring.php"); ?>><i class="bi bi-file-post"style="font-size:20px"></i>&nbsp;Urgent Hiring</a>
     <a href="applicant_find_jobs.php" <?php echo isActivePage("applicant_find_jobs.php"); ?>><i class="bi bi-search" style="font-size:20px"></i>&nbsp;Find Job</a>
     <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-bell" style="font-size:20px"></i>&nbsp;NOTIFICATION</a>
-    <a href="applicant_profile.php" <?php echo isActivePage("applicant_profile.php"); ?>><i class="bi bi-person-lines-fill" style="font-size:20px"></i>&nbsp;PROFILE</a>
+    <a href="af_personal_information.php" <?php echo isActivePage("af_personal_information.php"); ?>><i class="bi bi-person-lines-fill" style="font-size:20px"></i>&nbsp;PROFILE</a>
     <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-sliders2" style="font-size:20px"></i>&nbsp;SETTING</a>
     <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-envelope-at-fill" style="font-size:20px"></i>&nbsp;MORE INFO</a>
     <a href="sign_out.php" <?php echo isActivePage("sign_out.php"); ?>><i class="bi bi-box-arrow-left" style="font-size:20px"></i>&nbsp;LOG OUT</a>
